6f00c79472c9cf5044aa49011c3cbf2bd28aa0ea
Juulia Sarlio <fjusa016@edu.xamk.fi>
2025-08-20 20:36:58 +0300

    update

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
   hamburger.classList.toggle('active'); 
+
+   if (navLinks.classList.contains('active')) {
+    document.body.style.overflow = 'hidden';  
+  } else {
+    document.body.style.overflow = '';        
+  }
 });
 
 document.querySelectorAll('.lightbox-link').forEach(link => {
